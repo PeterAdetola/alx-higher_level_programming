4b949c138e1dd2f6e6a826ba51255314e4b87043
Peter Adetola <peteradetola@gmail.com>
2023-02-03 16:52:54 -0800

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,10 @@
-ay_my_name prints back the parameters
+#!/usr/bin/python3
 
-first_name followed by last_name
+"""
 
-last_name is defaulted to an empty string
+Module say_my_name
+
+Prints out first name and last name
 
 """
 
@@ -12,24 +14,26 @@ last_name is defaulted to an empty string
 
 def say_my_name(first_name, last_name=""):
 
-    """ Prints "My name is <first name> <last name>"
+    """
 
-    checks if first_name is a string
+    Prints out first name and last name
 
-    checks if last_name is a string
+    first name and last name must be of the type string
+
+    or error if not string
 
     """
 
-    if type(first_name) != str:
+
+
+    if type(first_name) is not str:
 
         raise TypeError("first_name must be a string")
 
-
-
-    if type(last_name) != str:
+    if type(last_name) is not str:
 
         raise TypeError("last_name must be a string")
 
+    
 
-
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
