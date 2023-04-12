c0cbdc2dca85e0a6903652aec1acba6a51aa2440
Peter Adetola <peteradetola@gmail.com>
2023-04-12 07:46:27 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-let arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i in arr) {
-    console.log(arr[i]);
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const i in array) {
+  console.log(array[i]);
 }
