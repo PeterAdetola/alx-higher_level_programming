bb6b8511e57488f3893a374c61e012cfca989dd4
Peter Adetola <peteradetola@gmail.com>
2023-04-12 07:28:17 -0700

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let arr = [
+    'C is fun',
+    'Python is cool',
+    'JavaScript is amazing'
+];
+
+console.log(arr.length);
+
+for (let i = 0; i < arr.length; i++ ) {
+    console.log(arr[i]);
+}
