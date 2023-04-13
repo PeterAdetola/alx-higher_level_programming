ad12974002d12be0d67a384575ab18c8e06e425a
Peter Adetola <peteradetola@gmail.com>
2023-04-13 11:15:27 -0700

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,10 @@
-i#!/usr/bin/node
+#!/usr/bin/node
 function factorial (n) {
-  if (n < 0) {
-    return (-1);
-  }
-  if (n === 0 || isNaN(n)) {
+  if (isNaN(n) || n === 1) {
     return (1);
+  } else {
+    return (n * factorial(n - 1));
   }
-  return (n * factorial(n - 1));
 }
 
-console.log(factorial(Number(process.argv[2])));
+console.log(factorial(Math.floor(Number(process.argv[2]))));
